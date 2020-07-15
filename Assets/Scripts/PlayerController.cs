904962d632d6fe3c620cd9fcc8792c8e3140778d
Komodeo <onlythebest@gmail.com>
2020-07-15 13:10:43 -0400

    camera follows player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
